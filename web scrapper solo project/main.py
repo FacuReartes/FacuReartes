f9fa0bae062f39e336758c11e81633d99f2433f7
FacuReartes <102768575+FacuReartes@users.noreply.github.com>
2022-03-31 19:19:10 -0300

    Update main.py

--- web scrapper solo project/main.py
+++ web scrapper solo project/main.py
@@ -2,15 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-"""
-university
-POStal
-website
-mail
-phone
-fax
-"""
-
 url = "https://americassbdc.org/about-us/state-directors/"
 html = requests.get(url).text
 soup = BeautifulSoup(html, "html.parser")
@@ -40,32 +31,6 @@ for i in chapters:
                 else:
                     data_dict["Fax"] = "There is no Fax"
         master_list.append(data_dict)
-#        print(f"Mail: {mail} | Website: {website} | {phone} | {tax}")
-
-"""
-for i in chapters:
-    data_dict = {}
-    nextsibling = a_tag = None
-    website = False
-    tax = "There is no fax"
-    a_tag = i.find_all("a")
-    if len(a_tag) > 1 and "@" in a_tag[0].text:
-        mail = a_tag[0].text
-        for j in a_tag:
-            if "." in j.text:
-                website = j.text
-        for br in i.find_all("br"):
-            nextsibling = br.nextSibling
-            if nextsibling and "Phone" in nextsibling:
-                sibling_split = nextsibling.split(";")
-                phone = sibling_split[0]
-                if len(sibling_split) > 1:
-                    tax = sibling_split[1]
-                else:
-                    tax = "There is no Fax"
-        master_list.append(data_dict)
-        print(f"Mail: {mail} | Website: {website} | {phone} | {tax}")
-"""
 
 
 excel = pd.DataFrame(master_list)
